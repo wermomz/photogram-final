760112bf7056d755fb5c43c77e3a0144041ebb37
litalorlev1323 <lorlev@chicagobooth.edu>
2020-11-26 03:56:34 +0000

    photo db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Photo resource:
+
+  # CREATE
+  post("/insert_photo", { :controller => "photos", :action => "create" })
+          
+  # READ
+  get("/photos", { :controller => "photos", :action => "index" })
+  
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_photo/:path_id", { :controller => "photos", :action => "update" })
+  
+  # DELETE
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
+
+  #------------------------------
+
   # Routes for the User account:
 
   # SIGN UP FORM
