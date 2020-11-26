b97cebc7ca67503c606a983dbde182ad90768443
litalorlev1323 <lorlev@chicagobooth.edu>
2020-11-26 03:58:34 +0000

    all DB

---
+++ app/models/followrequest.rb
@@ -0,0 +1,2 @@
+class Followrequest < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,61 @@
 Rails.application.routes.draw do
+  # Routes for the Like resource:
+
+  # CREATE
+  post("/insert_like", { :controller => "likes", :action => "create" })
+          
+  # READ
+  get("/likes", { :controller => "likes", :action => "index" })
+  
+  get("/likes/:path_id", { :controller => "likes", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_like/:path_id", { :controller => "likes", :action => "update" })
+  
+  # DELETE
+  get("/delete_like/:path_id", { :controller => "likes", :action => "destroy" })
+
+  #------------------------------
+
+  # Routes for the Followrequest resource:
+
+  # CREATE
+  post("/insert_followrequest", { :controller => "followrequests", :action => "create" })
+          
+  # READ
+  get("/followrequests", { :controller => "followrequests", :action => "index" })
+  
+  get("/followrequests/:path_id", { :controller => "followrequests", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_followrequest/:path_id", { :controller => "followrequests", :action => "update" })
+  
+  # DELETE
+  get("/delete_followrequest/:path_id", { :controller => "followrequests", :action => "destroy" })
+
+  #------------------------------
+
+  # Routes for the Comment resource:
+
+  # CREATE
+  post("/insert_comment", { :controller => "comments", :action => "create" })
+          
+  # READ
+  get("/comments", { :controller => "comments", :action => "index" })
+  
+  get("/comments/:path_id", { :controller => "comments", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_comment/:path_id", { :controller => "comments", :action => "update" })
+  
+  # DELETE
+  get("/delete_comment/:path_id", { :controller => "comments", :action => "destroy" })
+
+  #------------------------------
+
   # Routes for the Photo resource:
 
   # CREATE
